17faa8792878b15f324f3b08a8ac9d9e32ef5ecc
Victor <quanggvuu1004@gmail.com>
2023-04-28 19:18:08 +0700

    add image

--- components/Section/WhyBus/index.js
+++ components/Section/WhyBus/index.js
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export default function WhyBusSection() {
     return (
       <div className="px-x-global py-16">
         <div className="rounded-3xl px-20 py-8 flex items-center">
           <div className="">
             <Image
-              src={"/img/illustration_1.svg"}
+              src={"/img/illustration_2.svg"}
               height={400}
               width={400}
               className=""
